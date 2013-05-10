f562a125c6930c259d70196cdd0271c001fc9664
mvigil <student@mildrid.cs.ucsb.edu>
2013-05-10 15:32:37 -0700

    Added account login GUI.

--- OpportunisticOwnCloud/gen/com/example/opportunisticowncloud/R.java
+++ OpportunisticOwnCloud/gen/com/example/opportunisticowncloud/R.java
@@ -25,13 +25,22 @@ public final class R {
     }
     public static final class id {
         public static final int LinearLayout1=0x7f080000;
-        public static final int action_settings=0x7f080004;
-        public static final int button1=0x7f080001;
-        public static final int button2=0x7f080002;
-        public static final int listView1=0x7f080003;
+        public static final int action_settings=0x7f08000c;
+        public static final int button1=0x7f080007;
+        public static final int button2=0x7f080008;
+        public static final int button_download=0x7f08000a;
+        public static final int button_upload=0x7f080009;
+        public static final int editText1=0x7f080002;
+        public static final int editText2=0x7f080005;
+        public static final int editText3=0x7f080006;
+        public static final int imageView1=0x7f080001;
+        public static final int imageView2=0x7f080003;
+        public static final int list_files=0x7f08000b;
+        public static final int textView1=0x7f080004;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int account_setup=0x7f030000;
+        public static final int activity_main=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
