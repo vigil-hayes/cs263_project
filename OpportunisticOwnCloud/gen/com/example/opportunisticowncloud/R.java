1ab9eeee4de41c71111ff1dde10521ec2cd36ac4
mvigil <student@mildrid.cs.ucsb.edu>
2013-05-12 14:49:01 -0700

    Update random xml and project porperties.

--- OpportunisticOwnCloud/gen/com/example/opportunisticowncloud/R.java
+++ OpportunisticOwnCloud/gen/com/example/opportunisticowncloud/R.java
@@ -10,72 +10,49 @@ package com.example.opportunisticowncloud;
 public final class R {
     public static final class attr {
     }
-    public static final class dimen {
-        /**  Default screen margins, per the Android Design guidelines. 
-
-         Customize dimensions originally defined in res/values/dimens.xml (such as
-         screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
-    
-         */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
-    }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int back_icon=0x7f020000;
+        public static final int file_icon=0x7f020001;
+        public static final int folder_icon=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
     }
     public static final class id {
-        public static final int LinearLayout1=0x7f080000;
-        public static final int action_settings=0x7f08000c;
-        public static final int button1=0x7f080007;
-        public static final int button2=0x7f080008;
-        public static final int button_download=0x7f08000a;
-        public static final int button_upload=0x7f080009;
-        public static final int editText1=0x7f080002;
-        public static final int editText2=0x7f080005;
-        public static final int editText3=0x7f080006;
-        public static final int imageView1=0x7f080001;
-        public static final int imageView2=0x7f080003;
-        public static final int list_files=0x7f08000b;
-        public static final int textView1=0x7f080004;
+        public static final int about_layout=0x7f050000;
+        public static final int button_download=0x7f05000b;
+        public static final int button_upload=0x7f05000a;
+        public static final int editText1=0x7f050004;
+        public static final int itemlist=0x7f050007;
+        public static final int label_createfolder=0x7f050003;
+        public static final int local_picture=0x7f050008;
+        public static final int local_text=0x7f050009;
+        public static final int mLocalList=0x7f050006;
+        public static final int mOCList=0x7f05000c;
+        public static final int mServerList=0x7f05000d;
+        public static final int mText=0x7f050005;
+        public static final int scrollView1=0x7f050001;
+        public static final int server_picture=0x7f05000e;
+        public static final int server_text=0x7f05000f;
+        public static final int web_about=0x7f050002;
     }
     public static final class layout {
-        public static final int account_setup=0x7f030000;
-        public static final int activity_main=0x7f030001;
-    }
-    public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int about=0x7f030000;
+        public static final int create_new_file=0x7f030001;
+        public static final int create_new_folder=0x7f030002;
+        public static final int item=0x7f030003;
+        public static final int local_files=0x7f030004;
+        public static final int local_item=0x7f030005;
+        public static final int main=0x7f030006;
+        public static final int server_files=0x7f030007;
+        public static final int server_item=0x7f030008;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
-    }
-    public static final class style {
-        /** 
-        Base application theme, dependent on API level. This theme is replaced
-        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
-    
-
-            Theme customizations available in newer API levels can go in
-            res/values-vXX/styles.xml, while customizations related to
-            backward-compatibility can go here.
-        
-
-        Base application theme for API 11+. This theme completely replaces
-        AppBaseTheme from res/values/styles.xml on API 11+ devices.
-    
- API 11 theme customizations can go here. 
-
-        Base application theme for API 14+. This theme completely replaces
-        AppBaseTheme from BOTH res/values/styles.xml and
-        res/values-v11/styles.xml on API 14+ devices.
-    
- API 14 theme customizations can go here. 
-         */
-        public static final int AppBaseTheme=0x7f060000;
-        /**  Application theme. 
- All customizations that are NOT specific to a particular API-level can go here. 
-         */
-        public static final int AppTheme=0x7f060001;
+        public static final int about=0x7f040001;
+        public static final int app_title=0x7f040000;
+        public static final int cancel=0x7f040004;
+        public static final int finish=0x7f040002;
+        public static final int local_file=0x7f040007;
+        public static final int oc_file=0x7f040006;
+        public static final int ok=0x7f040005;
+        public static final int prompt=0x7f040003;
     }
 }
